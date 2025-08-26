4b926035f563c97e218dd734871ae1448040c217
surajit-tunga <surajittunga2005@gmail.com>
2025-08-26 20:18:13 +0530

    Create resume model & controller for CURD

---
+++ backend/controllers/resumeController.js
@@ -0,0 +1,183 @@
+import Resume from '../models/resumeModel.js';
+import fs from'fs';
+import path from 'path';
+
+
+// Create Resume
+export const createResume = async (req, res)=>{
+    try{
+        const {title}= req.body;
+        // DEFAULT TEMPLATE
+        const defaultResumeData = {
+            profileInfo: {
+                profileImg: null,
+                previewUrl: '',
+                fullName: '',
+                designation: '',
+                summary: '',
+            },
+            contactInfo: {
+                email: '',
+                phone: '',
+                location: '',
+                linkedin: '',
+                github: '',
+                website: '',
+            },
+            workExperience: [
+                {
+                    company: '',
+                    role: '',
+                    startDate: '',
+                    endDate: '',
+                    description: '',
+                },
+            ],
+            education: [
+                {
+                    degree: '',
+                    institution: '',
+                    startDate: '',
+                    endDate: '',
+                },
+            ],
+            skills: [
+                {
+                    name: '',
+                    progress: 0,
+                },
+            ],
+            projects: [
+                {
+                    title: '',
+                    description: '',
+                    github: '',
+                    liveDemo: '',
+                },
+            ],
+            certifications: [
+                {
+                    title: '',
+                    issuer: '',
+                    year: '',
+                },
+            ],
+            languages: [
+                {
+                    name: '',
+                    progress: '',
+                },
+            ],
+            interests: [''],
+        };
+        const newResume = await Resume.create({
+            userId: req.user._id,
+            title,
+            ...defaultResumeData,
+            ...req.body
+        }); 
+        res.status(201).json(newResume);
+    } catch(error){
+        res.status(500).json({
+            message: "Failed to create resume",
+            error: error.message
+        });
+    }
+}
+
+// Getting the Resumes
+export const getUserResumes =async(req, res)=>{
+    try{
+        const resume = await Resume.find({userId: req.user._id}).sort({
+            updatedAt: -1
+        });
+
+    } catch(error){
+        res.status(500).json({
+            message: "Failed to get resume",
+            error: error.message
+        });
+    }
+}
+
+//Get Resume by id
+export const getUserResumeById =async(req, res)=>{
+    try{
+        const resume = await Resume.findOne({_id: req.params.id, userId: req.user._id});
+        if(!resume){
+            return res.status(404).json({message: "Resume not found"});
+        } 
+        res.json(resume);
+    } catch(error){
+        res.status(500).json({
+            message: "Failed to get resume",
+            error: error.message
+        });
+    }
+}
+
+//Update Resume
+export const updateResume =async(req, res)=>{
+    try{
+        const resume = await Resume.findOne({_id: req.params.id, userID: req.user._id});
+        if(!resume){
+            return res.status(404).json({message: "Resume not found"});
+        } 
+        //update 
+        Object.assign(resume, req.body)
+        //Save
+        const saveResume = await resume.save();
+        res.json(saveResume);
+    } catch(error){
+        res.status(500).json({
+            message: "Failed to Update resume",
+            error: error.message
+        });
+    }
+}
+
+//Delete Resume
+export const deleteResume =async(req, res)=>{
+    try{
+        const resume = await Resume.findOne({_id: req.params.id, userID: req.user._id});
+        if(!resume){
+            return res.status(404).json({message: "Resume not found"});
+        } 
+
+        //Creates a uploads folder & store the reusme
+        const uploadsFolder =path.join(process.cwd(), 'uploads');
+
+        //Delete the thumbnil
+        if (resume.thumbnailLink){
+            const oldthumbnail = path.join(uploadsFolder, path.basename(resume.thumbnailLink));
+            if(fs.existsSync(oldthumbnail)){
+                fs.unlinkSync(oldthumbnail);
+            }
+        }
+
+        if (resume.profileInfo?.profilePreviewUrl) {
+            const oldProfile = path.join(
+                uploadsFolder,
+                path.basename(resume.profileInfo.profilePreviewUrl)
+            )
+            if(fs.existsSync(oldProfile)){
+                fs.unlinkSync(oldProfile);
+            }
+        }
+        
+        //Delete from DB
+        const deleted = await Resume.findOneAndDelete({
+            _id: req.params.id, 
+            userID: req.user._id
+        });
+        if (!deleted){
+            return res.status(404).json({message: "Resume not found"});
+        }
+        res.json ({message:"Resume Deleted Successfully"})
+    } catch(error){
+        res.status(500).json({
+            message: "Failed to Delete resume",
+            error: error.message
+        });
+    }
+}
--- backend/models/resumeModel.js
+++ backend/models/resumeModel.js
@@ -1,1 +1,78 @@
 import mongoose from  'mongoose';
+
+const ResumeSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    thumbnailLink: {
+        type: String
+    },
+    template: {
+        type: String,
+        colorPalette: [String],
+    },
+    profileInfo:{
+        profilePreviewUrl: String,
+        fullName: String,
+        designation: String,
+        summary: String,
+    },
+    contactInfo:{
+        email: String,
+        phone: String,
+        location: String,
+        linkedin: String,
+        github: String,
+        website: String,
+    },
+    // Work Experience
+    workExperience: [{
+        company: String,
+        role: String,
+        startDate: Date,
+        endDate: Date,
+        description: String,
+    }],
+    // Education
+    education: [{
+        degree: String,
+        institution: String,
+        startDate: Date,
+        endDate: Date,
+    }],
+    // Skills
+    skills: [{
+        name: String,
+        progress: Number,
+    }],
+   //Projects
+   projects: [{
+        title: String,
+        description: String,
+        github: String,
+        liveDemo: String,
+    }],
+    //Certification
+    certification: [{
+        title: String,
+        issuer: String,
+        year: String,
+    }],
+    languages: [{
+        name:String,
+        progress:Number,
+    }],
+    interests: [String],
+  },
+   { 
+    timestamps: {createdAt: "createdAt", updatedAt: "updatedAt"}
+   },
+);
+
+export default mongoose.model("Resume", ResumeSchema);
