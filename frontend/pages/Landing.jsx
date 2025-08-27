5b350643ed52d4171bac4562b2a1d6c5dd537aec
surajit-tunga <surajittunga2005@gmail.com>
2025-08-27 09:42:41 +0530

    Basic client setup & Import assets from github

---
+++ frontend/pages/Landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <div>
+        <p>kings Landing</p>
+    </div>
+  )
+}
+
+export default Landing
+
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Landing from '../pages/Landing'
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </div>
+    <Routes>
+      <Route path='/' element={<Landing/>} />
+    </Routes>
+    
   )
 }
 
