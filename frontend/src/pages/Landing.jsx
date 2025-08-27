5cbc44566b2fb20a13649c45626f52b6eabc766d
surajit-tunga <surajittunga2005@gmail.com>
2025-08-28 01:05:59 +0530

    Added Hero (Left side)

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,15 +1,25 @@
 import React, { useContext, useState } from 'react'
 import {useNavigate}  from 'react-router-dom'
 import {landingPageStyles} from '../assets/dummystyle'
-import { LayoutTemplate, Menu, X } from 'lucide-react'
+import { ArrowRight, LayoutTemplate, Menu, X } from 'lucide-react'
 import {UserContext} from '../context/UserContext'
 import { ProfileInfocard } from '../components/Cards'
 
 const Landing = () => {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-  const [openAuthModal, setOpenAuthModal] = useState(false);
   const {user} = useContext(UserContext);
   const navigate =useNavigate();
+
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [openAuthModal, setOpenAuthModal] = useState(false);
+  const [currentPage, setCurrentPage]= useState("login");
+
+  const handleCTA =()=>{
+    if(!user){
+      setOpenAuthModal(true)
+    } else {
+      navigate('/dashbord')
+    }
+  }
 
   return (
     <div className={landingPageStyles.container}>
@@ -72,6 +82,59 @@ const Landing = () => {
           </div>
          )}
       </header>
+
+      {/* MAIN CONTENT*/}
+      <main className={landingPageStyles.main} >
+        {/* Hero */}
+        <section className={landingPageStyles.heroSection}>
+          <div className={landingPageStyles.heroGrid}>
+            {/* LEFT CONTENT */}
+            <div className={landingPageStyles.heroLeft}>
+              <div className={landingPageStyles.tagline}>
+                Professional Resume Builder
+              </div>
+              <h1 className={landingPageStyles.heading}>
+                <span className={landingPageStyles.headingText}>Craft</span>
+                <span className={landingPageStyles.headingGradient}>Professional</span>
+                <span className={landingPageStyles.headingText}>Resumes.</span>
+              </h1>
+              <p className={landingPageStyles.description}>
+                Create job-winning Resume with expertly designd templates. ATS-friendly & Tailored to your career.
+              </p>
+
+              <div className={landingPageStyles.ctaButtons}>
+                <button className={ landingPageStyles.primaryButton} onClick={handleCTA}>
+                  <div className={landingPageStyles.primaryButtonOverlay}></div>
+                  <span className={landingPageStyles.primaryButtonContent}>
+                    Start Building
+                    <ArrowRight className={landingPageStyles.primaryButtonIcon}/>
+                  </span>
+                </button>
+                <button className={landingPageStyles.secondaryButton} onClick={handleCTA}> View Templates</button>
+              </div>
+              {/* Stat Grid */}
+              <div className={landingPageStyles.statsContainer}>
+                {[
+                  { value: '50K+', label: 'Resumes Created', gradient: 'from-violet-600 to-fuchsia-600' },
+                  { value: '4.9★', label: 'User Rating', gradient: 'from-orange-500 to-red-500' },
+                  { value: '5 Min', label: 'Build Time', gradient: 'from-emerald-500 to-teal-500' }
+                ].map((stat, idx)=>(
+                  <div className={landingPageStyles.statItem} key={idx}>
+                    <div className={`${landingPageStyles.statNumber} ${stat.gradient}`}>
+                      {stat.value}
+                    </div>
+                    <div className={landingPageStyles.statLabel}>
+                      {stat.label}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+            {/* /RIGHT CONTENT 2.02*/}
+          </div>
+        </section>
+      </main>
+
     
     </div>
   )
